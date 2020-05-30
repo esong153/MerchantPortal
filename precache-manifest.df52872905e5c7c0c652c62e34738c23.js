75096ca2bb0e552c85c3b75f5b2be81eeeb043bf
ericsong <eric15344@gmail.com>
2020-05-29 20:13:37 -0700

    Updates

---
+++ precache-manifest.df52872905e5c7c0c652c62e34738c23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45cfbb0e8446d6ed18cb49f55506d40f",
+    "url": "/MerchantPortal/index.html"
+  },
+  {
+    "revision": "ee6fdc5d684392378c06",
+    "url": "/MerchantPortal/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ea0509b7d6fb7d888b8e",
+    "url": "/MerchantPortal/static/css/main.3825b5f0.chunk.css"
+  },
+  {
+    "revision": "ee6fdc5d684392378c06",
+    "url": "/MerchantPortal/static/js/2.835b627d.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/MerchantPortal/static/js/2.835b627d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea0509b7d6fb7d888b8e",
+    "url": "/MerchantPortal/static/js/main.3d84067d.chunk.js"
+  },
+  {
+    "revision": "71c75891babd7d83a52c",
+    "url": "/MerchantPortal/static/js/runtime-main.f023ef32.js"
+  }
+]);
